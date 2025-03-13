a2d672c8f510f927c1d9310909be498d54b54855
Chineta Adinnu <chinetaadinnu@gmail.com>
2025-03-13 11:02:04 +0100

    update copyright

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -119,7 +119,7 @@ const Footer = () => {
         </div>
         <div className='mt-8 border-t border-gray-700 pt-8 md:flex md:items-center md:justify-center'>
           <p className='mt-8 text-base text-gray-400 md:mt-0 md:order-1'>
-            &copy; 2024 PreStayCheck. All rights reserved.
+          Copyright &copy; 2025 | Adblabla Technologies Incorporated
           </p>
         </div>
       </div>
